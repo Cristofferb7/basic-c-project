0e6d9f487c4dd0fff3dd5d0aeccebe7ef851c8d9
Cristoffer Bohorquez <cristoffersmacbook@Cristoffers-MacBook-Air.local>
2025-09-12 09:52:44 -0400

    first branch

---
+++ global_variables/main.c
@@ -0,0 +1,207 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+/*	•	%d → for int and bool
+	•	%c → for char
+	•	%f → for float
+	•	%lf → for double (long float)*/
+
+int main()
+
+{
+    
+    // printf ("follow the prompts \n");
+    // int x = 5;
+    // char c = 'c';
+    // float f = 3.14;
+    // double d = 22.2222;
+    // bool isDone = true;
+
+    // int userAge;
+    // float userGpa;
+
+    // printf("type your age\n");
+
+
+    // scanf("%d", &userAge);
+
+    // printf("type your gpa\n");
+    // scanf("%f", &userGpa);
+
+    // printf("you typed %d \n", userAge);
+    // printf("you added %f\n" , userGpa);
+
+
+
+
+    // printf ("%d\n", x);
+    // printf("%c\n", c);
+    // printf("%f\n", f);
+    // printf("%lf\n", d);
+    // printf("%d\n", isDone);]
+
+
+//    int yearBorn;
+//    double timeBorn;
+
+
+// printf ("Hey what year were you born \n");
+
+// scanf("%d" ,&yearBorn);
+
+// printf("Also what time ?\n");
+
+// scanf("%lf", &timeBorn);
+
+// printf("this is the year your were born %d\n" ,yearBorn);
+
+// printf("this is the time %lf\n", timeBorn);
+
+// int favNumber;
+
+// printf("Hi, Type your favorite number  \n");
+// scanf("%d", &favNumber);
+
+// int favNumberMultiplied = favNumber * 10;
+
+
+// printf ("Here's your number multiplied   %d", favNumberMultiplied);
+
+
+
+// float userHeight;
+
+// printf("Hey! can you please enter your height with decimals \n");
+
+// scanf("%f",&userHeight);
+
+// printf("this is your height with decimals %.2f" ,userHeight);
+
+
+// float sleep1;
+// float sleep2;
+
+// printf("How many hours of sleep did you get last night? ");
+// scanf("%f", &sleep1);
+
+// printf("now how many hours do you want to sleep tonight");
+// scanf("%f",&sleep2);
+
+// float average = (sleep1+sleep2)/2;
+
+// printf("this is how many hours you going to average %.2f", average);
+
+
+
+// int userAge;
+
+// printf("how old are you?\n");
+// scanf("%d",&userAge);
+
+// int result = userAge * 12;
+
+// printf("%d\n" , result);
+
+
+// return 0;
+
+// float totalBill;
+
+// printf("Hey how much was your bill?\n");
+// scanf("%f",&totalBill);
+
+// float tip;
+// printf("how much tip would you like to leave ?\n");
+// scanf("%f", &tip);
+
+// float totalTip = totalBill *tip;
+
+// printf("this is how much your tip is %.2f \n", totalTip);
+
+// return 0;
+
+// int celsius;
+
+// printf("enter the celsius please \n");
+// scanf("%d",&celsius);
+
+// float F = (celsius * 9/5) + 32;
+// printf("this is the result %.2f \n", F);
+
+// return 0;
+
+// int number1;
+// int number2;
+
+// printf("please put number 1 \n");
+// scanf("%d",&number1);
+
+// printf("please put number 2 \n");
+// scanf("%d", &number2);
+
+// int remainder = number1 % number2;
+
+// printf("here's your remainder %d \n", remainder);
+
+// return 0;
+
+// int number1;
+
+// printf("enter a number\n");
+// scanf("%d", &number1);
+
+// if (number1 % 2 == 0)
+// {
+//     printf("number is even \n");
+    
+// }
+// else if  (number1 % 2  != 0)
+// {
+
+//     printf("number is not even \n");
+
+// }
+
+// return 0;
+
+
+
+// int main()
+// {
+// float price;
+
+//     printf("whats the price for that item?\n");
+//     scanf("%f",&price);
+
+//     printf("this is the price you added %10.2f", price);
+
+//     return 0;
+// }
+
+// float bill;
+// float tip;
+// {
+// printf("ask the user for the bill\n");
+// scanf("%f", &bill);
+
+// printf("ask the user for how much they want to tip\n");
+// scanf("%f", &tip);
+
+// float total = bill + tip;
+// printf("this is the total %10.2f\n", total);
+
+// }
+
+char c = 'A';
+
+printf("char c: %d\n",c);
+printf("char c: %c\n",c);
+
+c = 66;
+printf("char c: %c\n",c);
+
+};
+
+
+
---
+++ mini_project_using_loops/main.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int sleepHours;
+char userSelection;
+float totalSleep;
+float totalHours;
+
+
+
+int main()
+{
+
+    while (userSelection != 'C' && 'c' != userSelection)
+    {
+    printf("welcome to the sleep tracker, please select one of these options to check: \n");
+    printf("A to enter sleep\n");
+    printf("B to check total sleep\n");
+    printf("C to exit\n");
+
+    userSelection = getchar(); //wait for the user to type a character
+    getchar(); //deletes the next line
+
+    if (userSelection == 'A' || userSelection == 'a')
+
+{
+    printf("please enter sleep\n");
+    scanf("%d",&sleepHours);
+    getchar();
+    totalSleep+= sleepHours;
+    printf("this is the amount of hours you entered %d", sleepHours);
+    getchar();
+
+    
+
+
+    }
+
+    else if (userSelection == 'B' || userSelection == 'b')
+    {
+        printf("you have selected to check total sleep\n");
+        printf("this is your total sleep %f" ,totalSleep);
+        getchar();
+    }
+
+
+
+
+    
+
+}
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
